6ffaf13c9fc7153033c294384f80f403dad0d495
lakshyakumar1509 <119105999+lakshyakumar1509@users.noreply.github.com>
2024-03-26 11:52:49 +0530

    Add files via upload

---
+++ Day 6/EventApplication.java
@@ -0,0 +1,14 @@
+package com.java.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @RequestMapping("/wrapit/api/v1")
+@SpringBootApplication
+public class EventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventApplication.class, args);
+	}
+
+}
